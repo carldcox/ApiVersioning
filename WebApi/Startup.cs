0d5d886930f9f1a491e0c7d6b4c85b26ec4fd52f
pseudo <pseudo.god333@gmail.com>
2022-01-24 13:01:09 -0500

    Adding in examples to change how the version is set from the client.

--- WebApi/Startup.cs
+++ WebApi/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Versioning;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -34,6 +35,21 @@ namespace WebApi
                 setup.DefaultApiVersion = new ApiVersion(1, 0);//Default Api Version
                 setup.AssumeDefaultVersionWhenUnspecified = true;//Make any unversioned call use the default
                 setup.ReportApiVersions = true;//Adds api-supported-versions header to all responses
+                
+                /*Change how we get the api version*/
+                //Default is url segment so this line is redundant
+                setup.ApiVersionReader = new UrlSegmentApiVersionReader();
+                
+                //Query Parameter, default parm name is api-version, here we are changing that to v
+                setup.ApiVersionReader = new QueryStringApiVersionReader("v");
+                
+                //Header
+                setup.ApiVersionReader = new HeaderApiVersionReader("api-version");
+                
+                //ContentType
+                // Content-type: application/json;v=1.0 --default
+                // Customized to Content-Type: application/json;version=1.0
+                setup.ApiVersionReader = new MediaTypeApiVersionReader("version");
             });
             
             services.AddSwaggerGen(c => { c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebApi", Version = "v1" }); });
